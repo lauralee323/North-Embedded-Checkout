c76360ee8143b7333cb242421a45170ec74c458f
lauralolson <lolson@nabancard.com>
2026-04-14 17:43:15 -0400

    updated transaction amount

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -9,11 +9,11 @@ export async function load({}: any) {
 				profileId: privateEnv.PRIVATE_PROFILE_ID,
 				metadata: 'Test metadata',
 				fields: ['cardField', 'cvvField'],
-				amount: 1,
+				amount: 1.05,
 				products: [
 					{
 						name: 'Product 1',
-						price: 1,
+						price: 1.05,
 						quantity: 1,
 						logoUrl:
 							'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/logo-organic-product-design-template-3abf71131287c02170f994587fb56eb1_screen.jpg?ts=1649667032'
